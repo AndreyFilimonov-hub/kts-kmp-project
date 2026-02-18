357aba04935f295ef235d6c39de8da016c5b2b2a
Andrey Filimonov <andrfil05@mail.ru>
2026-02-18 18:44:55 +0600

    Remove default generated classes

--- composeApp/src/commonMain/kotlin/com/filimonov/project/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.filimonov.project
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/filimonov/project/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.filimonov.project
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
