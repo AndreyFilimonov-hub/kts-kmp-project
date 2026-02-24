95c4cb962b3122a723c3c2b134571d1ee93126a2
Andrey Filimonov <andrfil05@mail.ru>
2026-02-24 19:17:24 +0600

    use AppTheme

--- composeApp/src/commonMain/kotlin/com/filimonov/project/App.kt
+++ composeApp/src/commonMain/kotlin/com/filimonov/project/App.kt
@@ -1,14 +1,14 @@
 package com.filimonov.project
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.filimonov.project.navigation.NavGraph
+import com.filimonov.project.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    AppTheme {
         NavGraph()
     }
 }
