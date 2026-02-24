09614eaac8a40e0e9e7e7c311fdcf9c1a5067566
Andrey Filimonov <andrfil05@mail.ru>
2026-02-24 19:16:23 +0600

    create utils Spacers

---
+++ composeApp/src/commonMain/kotlin/com/filimonov/project/ui/utils/Spacers.kt
@@ -0,0 +1,17 @@
+package com.filimonov.project.ui.utils
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MediumVerticalSpacer(modifier: Modifier = Modifier) {
+    Spacer(modifier = modifier.height(16.dp))
+}
+
+@Composable
+fun LargeVerticalSpacer(modifier: Modifier = Modifier) {
+    Spacer(modifier = modifier.height(32.dp))
+}
