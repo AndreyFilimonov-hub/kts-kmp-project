0d50ff5433b1cce637aa1f8fc0fa95308cac1db0
Andrey Filimonov <andrfil05@mail.ru>
2026-02-25 19:46:43 +0600

    create OnboardingPage

---
+++ composeApp/src/commonMain/kotlin/com/filimonov/project/presentation/screens/onboardingscreen/OnboardingPage.kt
@@ -0,0 +1,9 @@
+package com.filimonov.project.presentation.screens.onboardingscreen
+
+import org.jetbrains.compose.resources.DrawableResource
+
+data class OnboardingPage(
+    val imageRes: DrawableResource,
+    val title: String,
+    val description: String
+)
