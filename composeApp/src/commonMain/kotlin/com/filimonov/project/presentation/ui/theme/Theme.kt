145e8f4050b1c5768593a6aada6bdc6546fa222b
Andrey Filimonov <andrfil05@mail.ru>
2026-02-25 19:45:33 +0600

    replace green theme to blue

--- composeApp/src/commonMain/kotlin/com/filimonov/project/presentation/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/filimonov/project/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.filimonov.project.ui.theme
+package com.filimonov.project.presentation.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
